38ddcf38a0dc8afa99e074a56216e2674abd7ba0
Luis Acevedo <acevedla@bc.edu>
2019-01-17 12:41:18 -0500

    added second fetch call to get json data for museum piece

--- main.js
+++ main.js
@@ -16,11 +16,38 @@ function getMuseumID() {
         }
         throw new Error(response.statusText);
     })
-    .then(responseJson => console.log(getRandomMuseumId(responseJson.objectIDs)))
+    .then(responseJson => {
+    const rand = getRandomMuseumId(responseJson.objectIDs);
+    const finalMuseumUrl = museumIDUrl + '/' + rand;
+
+    console.log(finalMuseumUrl);
+
+    fetch(finalMuseumUrl)
+    .then(response => {
+        if (response.ok) {
+            return response.json();
+        }
+        throw new Error(response.statusText);
+    })
+    .then(responseJson => console.log(responseJson))
     .catch(err => {
         $('#error-message').text('woops something went wrong');
-    });
-    
+    });   
+})
+    .catch(err => {
+        $('#error-message').text('woops something went wrong');
+    });   
+}
+
+function displayRandomImage(responseJson) {
+    $('#met-data').empty();
+    for (let x = 0; x < responseJson.length; x++) {
+        $('#met-data').append(
+            `<li>
+            <h3>${responseJson[x].title}</h3>
+            <img src='${responseJson[x].primaryImage}' alt='no image availible'>
+            </li>`
+            )};
 }
 
 function watchForm() {
